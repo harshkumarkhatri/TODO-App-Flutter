2adb7036b21c79ccd27f32292e890b0d5f9e9cfa
Harsh Kumar Khatri <mailharshkhatri@gmail.com>
2020-10-22 13:38:06 +0530

    Items can be added, deleted & modified

--- lib/main.dart
+++ lib/main.dart
@@ -31,8 +31,28 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final myController = TextEditingController();
+  final myController2 = TextEditingController();
+
   List<Map<String, dynamic>> _queryRows;
   List data;
+  bool _visible = false;
+  bool _editVisibility = false;
+  int toUpdateId = 0;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    init();
+
+    super.initState();
+  }
+
+  void init() async {
+    _queryRows = await DatabaseHelper.instance.queryAll();
+    setState(() {
+      _queryRows;
+    });
+  }
 
   @override
   void dispose() {
@@ -45,16 +65,52 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Retrieve Text Input'),
+        title: Text('TODO Adder'),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 15),
+            child: GestureDetector(
+                onTap: () {
+                  setState(() {
+                    _editVisibility = false;
+                    _visible == false ? _visible = true : _visible = false;
+                  });
+                },
+                child: Icon(Icons.add)),
+          )
+        ],
       ),
       body: Column(
         children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextField(
-              controller: myController,
-            ),
-          ),
+          _editVisibility == true
+              ? Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextField(
+                    decoration:
+                        InputDecoration(hintText: "Enter text to replace"),
+                    controller: myController2,
+                  ),
+                )
+              : Container(),
+          _visible == true
+              ? Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextField(
+                    decoration:
+                        InputDecoration(hintText: "Enter new item to add"),
+                    controller: myController,
+                  ),
+                )
+              : Padding(
+                  padding: EdgeInsets.all(9),
+                  child: Container(
+                    child: Center(
+                      child: Text(
+                        "Add new items clicking on the + button.",
+                      ),
+                    ),
+                  ),
+                ),
           // makingList(_queryRows)
           Expanded(
             child: Container(
@@ -62,19 +118,52 @@ class _MyHomePageState extends State<MyHomePage> {
                 itemCount: _queryRows == null ? 0 : _queryRows.length,
                 itemBuilder: (BuildContext context, int index) {
                   return Padding(
-                    padding: const EdgeInsets.only(left:15,right:15),
+                    padding: const EdgeInsets.only(left: 15, right: 15),
                     child: new Card(
-                      child: Row(
+                      child: Column(
                         children: [
-                          new Text(
-                            _queryRows[index]["name"],
-                            style: TextStyle(
-                              color: Colors.red,
-                              fontSize: 24,
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              children: [
+                                new Text(
+                                  _queryRows[index]["name"],
+                                  style: TextStyle(
+                                    color: Colors.red,
+                                    fontSize: 24,
+                                  ),
+                                ),
+                                Spacer(),
+                                GestureDetector(
+                                  onTap: () {
+                                    setState(() {
+                                      toUpdateId = _queryRows[index]["_id"];
+                                      print("$toUpdateId is to update id");
+                                      _editVisibility == false
+                                          ? _editVisibility = true
+                                          : _editVisibility = false;
+                                      _visible = false;
+                                    });
+                                  },
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(right: 10),
+                                    child: Icon(Icons.edit),
+                                  ),
+                                ),
+                                GestureDetector(
+                                    onTap: () {
+                                      print("${_queryRows} is queryRows");
+                                      print(
+                                          "${_queryRows[index]} is index wala");
+                                      print(
+                                          "${_queryRows[index]["id"]} is passed");
+                                      deleteItem(_queryRows[index]["_id"]);
+                                    },
+                                    child: Icon(Icons.delete,
+                                        color: Colors.red, size: 30)),
+                              ],
                             ),
                           ),
-                          Spacer(),
-                          Icon(Icons.delete, color: Colors.red),
                         ],
                       ),
                     ),
@@ -89,10 +178,19 @@ class _MyHomePageState extends State<MyHomePage> {
         // When the user presses the button, show an alert dialog containing
         // the text that the user has entered into the text field.
         onPressed: () async {
-          int i = await DatabaseHelper.instance
-              .insert({DatabaseHelper.columnName: myController.text});
+          if (myController.text.length > 0) {
+            int i = await DatabaseHelper.instance
+                .insert({DatabaseHelper.columnName: myController.text});
 
-          print("The inserted id is $i");
+            print("The inserted id is $i");
+          }
+          if (myController2.text.length > 0) {
+            int updatedId = await DatabaseHelper.instance.update({
+              DatabaseHelper.columnId: toUpdateId,
+              DatabaseHelper.columnName: myController2.text
+            });
+            print("Updated id is $updatedId");
+          }
           // return showDialog(
           //   context: context,
           //   builder: (context) {
@@ -110,7 +208,7 @@ class _MyHomePageState extends State<MyHomePage> {
             _queryRows = queryRows;
           });
 
-          print("query rows are ${_queryRows[0]}");
+          print("query rows are ${_queryRows}");
           print("${queryRows.runtimeType} is runtime type");
           clearTextInput();
           print(_queryRows.length);
@@ -123,9 +221,19 @@ class _MyHomePageState extends State<MyHomePage> {
 
   clearTextInput() {
     myController.clear();
+    myController2.clear();
   }
 
   Widget makingList(queryRows) {
     return ListView.builder(itemCount: 3, itemBuilder: (context, index) {});
+  }
+
+  deleteItem(indexOfItem) async {
+    print("index of item is ${indexOfItem + 1}");
+    await DatabaseHelper.instance.delete(indexOfItem);
+    _queryRows = await DatabaseHelper.instance.queryAll();
+    setState(() {
+      _queryRows;
+    });
   }
 }
